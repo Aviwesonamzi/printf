f6007240e21ddebcf04046c843b6e199de041597
Aviwesonamzi <aviwesonamzi1@gmail.com>
2023-08-25 02:01:15 -0700

    0x11. C - printf

--- main.h
+++ main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H
+#define _MAIN_H
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -28,16 +28,27 @@ struct fmt
 	int (*fn)(va_list, char[], int, int, int, int);
 };
 
+/**
+ * typedef struct fmt fmt_t - Struct op
+ *
+ * @fmt: The format.
+ * @fm_t: The function associated.
+ */
 typedef struct fmt fmt_t;
+
 int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
 va_list list, char buffer[], int flags, int width, int precision, int size);
+
+
 int print_char(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_string(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_percent(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
+
+
 int print_int(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_binary(va_list types, char buffer[],
@@ -53,18 +64,30 @@ int print_hexa_upper(va_list types, char buffer[],
 
 int print_hexa(va_list types, char map_to[],
 char buffer[], int flags, char flag_ch, int width, int precision, int size);
+
+
 int print_non_printable(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
+
+
 int print_pointer(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
+
+
 int get_flags(const char *format, int *i);
 int get_width(const char *format, int *i, va_list list);
 int get_precision(const char *format, int *i, va_list list);
 int get_size(const char *format, int *i);
+
+
 int print_reverse(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
+
+
 int print_rot13string(va_list types, char buffer[],
-	int flags, int width, int precision, int size);	
+	int flags, int width, int precision, int size);
+
+
 int handle_write_char(char c, char buffer[],
 	int flags, int width, int precision, int size);
 int write_number(int is_positive, int ind, char buffer[],
@@ -77,6 +100,8 @@ int write_pointer(char buffer[], int ind, int length,
 int write_unsgnd(int is_negative, int ind,
 char buffer[],
 	int flags, int width, int precision, int size);
+
+
 int is_printable(char);
 int append_hexa_code(char, char[], int);
 int is_digit(char);
